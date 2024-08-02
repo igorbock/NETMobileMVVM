09b839a3593e4b1609bbbeaf5b91d0ee2f93d44c
igorbdaluz <igorbdaluz@hotmail.com>
2024-08-01 23:34:24 -0300

    Compilando os projetos completamente em repositórios separados. #8

--- MauiCRUD/Navigator/NavigatorMAUI.cs
+++ MauiCRUD/Navigator/NavigatorMAUI.cs
@@ -4,6 +4,11 @@ namespace MauiCRUD.Navigator;
 
 public class NavigatorMAUI : INavigationManager
 {
+    public void ClosePage()
+    {
+        throw new NotImplementedException();
+    }
+
     public async void ShowPage(string pageName)
     {
         if (string.IsNullOrEmpty(pageName)) return;
